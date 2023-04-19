a3b659f9166fbc17e47c26b9449f32a07bf74e74
Chinnikeerthi <keerthireddyg098@gmail.com>
2023-04-19 10:23:10 +0530

    first commit

---
+++ keer.py
@@ -0,0 +1,15 @@
+a=int(input('enter the marks'))
+if (a>=91 and a<=100):
+    print('A+')
+elif (a>=81 and a<=90):
+    print('A')
+elif (a>=71 and a<=80):
+    print('B')
+elif (a>=61 and a<=70):
+    print('C')
+elif (a>=51 and a<=60):
+    print('D')
+elif (a>=41 and a<=50):
+    print('E')
+else:
+    print('fail')                        
